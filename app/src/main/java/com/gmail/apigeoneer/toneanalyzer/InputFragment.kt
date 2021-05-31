e09877c309b948ffdad8672bf5fecd81faaa4188
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-31 10:18:38 +0530

    Added IBM Watson dependency

--- app/src/main/java/com/gmail/apigeoneer/toneanalyzer/InputFragment.kt
+++ app/src/main/java/com/gmail/apigeoneer/toneanalyzer/InputFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.gmail.apigeoneer.toneanalyzer.databinding.FragmentInputBinding
 
 class InputFragment : Fragment() {
+
+    private lateinit var binding: FragmentInputBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_input, container, false)
+        return binding.root
     }
 }
